656938b889d91fb05a21c4b72405e30045167f8c
Oren Mittman <nihil2501@gmail.com>
2023-01-09 21:15:40 -0500

    persisted badge

--- js/mode/core.js
+++ js/mode/core.js
@@ -4,17 +4,25 @@ import * as Storage from "./core/storage.js";
 export const restore = async ({ hostname, tabId }) => {
   const active = await Storage.fetch(hostname);
   if (active) {
-    Tab.update({ tabId, active });
+    Tab.update({
+      persisted: true,
+      tabId,
+      active,
+    });
   }
 };
 
 export const fullUpdate = async ({ hostname, tabId }) => {
-  const active = !await Tab.getCurrentActive({ tabId });
+  const active = !await Tab.getActive({ tabId });
   Storage.update({ hostname, active });
-  Tab.updateAll({ hostname, active });
+  Tab.updateAll({
+    persisted: true,
+    hostname,
+    active,
+  });
 };
 
 export const simpleUpdate = async ({ tabId }) => {
-  const active = !await Tab.getCurrentActive({ tabId });
+  const active = !await Tab.getActive({ tabId });
   Tab.update({ tabId, active });
 };
--- js/mode/core/tab.js
+++ js/mode/core/tab.js
@@ -2,46 +2,58 @@ const cssFile = "css/style.css";
 const activeProperties = {
   true: {
     changeCSS: "insertCSS",
-    file: "/icons/action/active.png",
-    title: "Invertus (active)",
+    iconPath: "/icons/action/active.png",
+    iconTitle: "Invertus (active)",
+    persistedBadgeText: "*",
   },
   false: {
     changeCSS: "removeCSS",
-    file: "/icons/action/inactive.png",
-    title: "Invertus (inactive)",
+    iconPath: "/icons/action/inactive.png",
+    iconTitle: "Invertus (inactive)",
+    persistedBadgeText: "",
   },
 };
 
-export const updateAll = async ({ hostname, active }) => {
+export const updateAll = async ({ hostname, active, persisted }) => {
   // This may not be strictly accurate for difference between `host` and
   // `hostname` which I think is `username:login`.
   const query = { url: hostnameMatchPattern(hostname) };
   const tabs = await chrome.tabs.query(query);
 
   for (const { id: tabId } of tabs) {
-    update({ tabId, active });
+    update({ tabId, active, persisted });
   }
 };
 
-export const update = ({ tabId, active }) => {
+// Need better conceptualization of the `persisted` stuff.
+export const update = async ({ tabId, active, persisted }) => {
   const properties = activeProperties[active];
 
-  chrome.scripting[properties.changeCSS]({
-    files: [cssFile],
-    target: { tabId },
-    origin: chrome.scripting.StyleOrigin.USER,
-  });
+  // To not over-insert the CSS.
+  const previousActive = await getActive({ tabId });
+  if (active !== previousActive) {
+    chrome.scripting[properties.changeCSS]({
+      files: [cssFile],
+      target: { tabId },
+      origin: chrome.scripting.StyleOrigin.USER,
+    });
+  }
 
-  const path = properties.file;
+  const path = properties.iconPath;
   chrome.action.setIcon({ tabId, path });
 
-  const title = properties.title;
+  const title = properties.iconTitle;
   chrome.action.setTitle({ tabId, title });
+
+  if (persisted) {
+    const text = properties.persistedBadgeText;
+    chrome.action.setBadgeText({ tabId, text });
+  }
 };
 
-export const getCurrentActive = async (details) => {
+export const getActive = async (details) => {
   const title = await chrome.action.getTitle(details);
-  return title === activeProperties.true.title;
+  return title === activeProperties.true.iconTitle;
 };
 
 const hostnameMatchPattern = (hostname) => {
